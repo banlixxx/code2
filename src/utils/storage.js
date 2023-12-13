bcdd18076bca02ad00d222f5759189988100b7de
banli <1844483502@qq.com>
2023-12-13 14:46:06 +0800

    12.13

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,5 @@
 const INFO_KEY = 'hm_shopping_info'
+const HISTORY_KEY = 'hm_shopping_history'
 
 // 获取个人信息
 export const getInfo = () => {
@@ -19,4 +20,18 @@ export const setInfo = (info) => {
 // 移除个人信息
 export const removeInfo = () => {
   localStorage.removeItem(INFO_KEY)
+}
+
+// 获取历史记录
+export const getHistory = () => {
+  const result = localStorage.getItem(HISTORY_KEY)
+  return result ? JSON.parse(result) : []
+}
+
+export const setHistory = (history) => {
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(history))
+}
+
+export const removeHistory = () => {
+  localStorage.removeItem(HISTORY_KEY)
 }
